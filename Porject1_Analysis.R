1d84ee84ef57f9a262728aca3e72b18d16080bbf
ZhaolinYang-maker <zhaolin.yang@mail.mcgill.ca>
2026-08-07 09:08:58 +0800

    Update Porject1_Analysis.R

--- Porject1_Analysis.R
+++ Porject1_Analysis.R
@@ -1,2 +1,41 @@
-11sada
-woshiyitouzhusd
+# Secondary Data Analysis Project1: Studying the Relationship between sleep deprivation and emotional regualtion and relationship between psychological states and cognitive performance
+# Aim 1: Association between sleepiness-related measurements and emotional regulation
+
+# Read the file from the local computer 
+data <- read.csv("/Users/zhaolinyang/Desktop/Indpendent Project/Project 1st/Dataset.csv")
+
+#=====================================================================================================================
+# MODULE1: Dataset Overview and Descriptive Statistics
+#=====================================================================================================================
+
+# STEP1: Brief overall summary for the whole data
+dim(data) # Quick check for the number of sample size (N = 60) and number of variables (p = 14)
+names(data) # Labeling variable names. Especially useful for quick check for if there is any misspelling in code
+summary(data) # Generate a brief descriptive summary of all variables
+
+#Checking missing values in the whole data
+colSums(is.na(data)) #Confirmation as described by the authors, there is no missing value in the dataset
+sum(duplicated(data)) #Checking for any duplicated rows in the data 
+
+# STEP2 Focusing on variables of interest with description and summary
+# Seven variables of interest included in the two aims. Mainly focusing on the sample statistics of the 7 variables
+variables <- c("Sleep_Hours", 
+               "Sleep_Quality_Score", 
+               "Emotion_Regulation_Score",
+               "Stress_Level",
+               "Stroop_Task_Reaction_Time",
+               "N_Back_Accuracy",
+               "PVT_Reaction_Time")
+
+mean_values <- sapply(data[variables], mean) # Computation of each variable's mean
+variance_values <- sapply(data[variables], var) # Computation of each variable's variance in sample
+SD_values <- sapply(data[variables], sd) #Computation of each variable's standard deviation in sample
+descriptive_statistics_table <-cbind(Mean = mean_values,
+                                     variance = variance_values,
+                                     SD = SD_values)
+round(descriptive_statistics_table,2) #Combine the values to be a table for summary and comparison
+
+#=====================================================================================================================
+# MODULE2: Necessary Data Preparation prior to formal data analysis
+#=====================================================================================================================
+
