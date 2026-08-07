85b37c4273d8775f80d36851f9d2d8eb6a456297
ZhaolinYang-maker <zhaolin.yang@mail.mcgill.ca>
2026-08-07 10:20:03 +0800

    Update Porject1_Analysis.R

--- Porject1_Analysis.R
+++ Porject1_Analysis.R
@@ -39,3 +39,28 @@ round(descriptive_statistics_table,2) #Combine the values to be a table for summ
 # MODULE2: Necessary Data Preparation prior to formal data analysis
 #=====================================================================================================================
 
+# STEP1: Generation of mean-centered variables (sleep-hours, sleep quality and emotional regulation score for AIM2)
+data$c_sleep_hours <- data$Sleep_Hours - mean(data$Sleep_Hours) # Mean-centering of sleep duration in a newly created column
+data$c_sleep_quality <- data$Sleep_Quality_Score - mean(data$Sleep_Quality_Score) # Mean-centering of sleep quality in a newly created column
+data$c_emotion_regulation <- data$Emotion_Regulation_Score - mean(data$Emotion_Regulation_Score) # Mean-centering of emotion regulation in a newly created column
+
+#STEP2 Converting continous stress level to categorical variable with three groups based on criteria of level of stress (Perceived Stress Scale PSS-10)
+# PSS-10 evaluates stress levels on a 0 to 40 scale. It classifies low stress group (0-10), moderate stress (14-26) and high stress group (27-40)
+
+data$Moderate_stress <- ifelse(14 <= data$Stress_Level
+                             &data$Stress_Level <= 26, 
+                             1, 0)
+data$High_stress <- ifelse(27 <= data$Stress_Level&
+                                data$Stress_Level <= 40, 
+                              1, 0)
+data$Stress_Group <- ifelse(data$Moderate_stress == 1, #Classification of different stress groups
+                               "Moderate Stress",
+                               ifelse(data$High_stress == 1,
+                                      "High Stress",
+                                      "Low Stress"
+                                      )
+                            ) 
+data$Stress_Group <- factor(data$Stress_Group,
+                            levels = c("Low Stress", "Moderate Stress", "High Stress")
+                            ) #Low stress group (stress level: 0-13) is baseline group of reference for later ANCOVA
+
